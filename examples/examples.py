92eadbe04aba563defc5798fbc7829d7d6b15a19
allen-r-d <108460258+allen-r-d@users.noreply.github.com>
2023-05-09 21:50:12 +0200

    Create examples.py
    initial commit of example file

---
+++ examples/examples.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  9 12:41:43 2023
+
+@author: rebeccaa
+"""
+
+def myVCCmodel():
+    
+    
+    return stuff
+
+
+
+def myOtherDef():
+    
+    
+    return otherStuff
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    #do these lines, that may or maynot call on above definitions
+    
+    # example 1: show basic functionality (use of different refrigerants, source/sink fluids, use of different libraries)
+    
+    
+    
+    # example 2: show how to include compressor data (n_comp(PR))
+    
+    
+    
+    # example 3: apply to practical problem of interest
+    #  - find cycle that produces desired Qcond, and report COP & compressor speed
+    # WORK ON THIS FIRST! THEN EX 1 AND 2 WILL BE EASIER TO DO.
+    
